be7cdd30f8c1d21f2fd5941e6cd7cc6b71e70bde
fnnardi <nardifiorella05@gmail.com>
2022-04-08 02:16:42 -0300

    Limpio comentarios

--- src/index.js
+++ src/index.js
@@ -4,16 +4,6 @@ import "./index.css";
 import { App } from "./App";
 
 
-// function Componente({titulo, children}){
-//  // const titulo = prop.titulo;
-//   //const contenido = prop.contenido;
-//   //const {titulo, contenido} = prop;
-//   return (
-//     <div>
-//       <h1>{titulo}</h1>
-//       <div>{children}</div>
-//     </div>
-//   )
-// }
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
